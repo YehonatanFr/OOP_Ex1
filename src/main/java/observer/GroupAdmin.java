b4798089a32f53ce35585554d615fc41f408d0fd
hagayknoro <54978956+hagayknoro@users.noreply.github.com>
2022-12-30 14:43:07 +0200

    partial java dok to GroupAdmin

--- src/main/java/observer/GroupAdmin.java
+++ src/main/java/observer/GroupAdmin.java
@@ -14,23 +14,40 @@ public class GroupAdmin implements Sender {
         this.usb = new UndoableStringBuilder();
     }
 
+    /**
+     * function that gives us the current number of members in the memberList in order to allow us to run tests on tha register function
+     * @return number of members.
+     */
     public int MembersCurrently()
     {
         return MemberList.size();
     }
 
+    /**
+     * Implements register from Sender interface (observer).
+     * @param obj adding the member that created by ConcreteMember to the MemberList.
+     */
     @Override
     public void register(Member obj) {
         this.MemberList.add(obj);
         System.out.println("Great to have you as a Member: "+obj.toString());
     }
 
+    /**
+     * Implements unregister from Sender interface (observer).
+     * @param obj = removing the member that the function gets from the MemberList.
+     */
     @Override
     public void unregister(Member obj) {
         this.MemberList.remove(obj);
         System.out.println("Hope you will register in the future: "+obj.toString());
     }
 
+    /**
+     *  Implements unregister from Sender interface (observer).
+     * @param offset = the index to start in implementing the new strung in usb
+     * @param obj = the index to end in implementing the new strung in usb
+     */
     @Override
     public void insert(int offset, String obj) {
         usb.insert(offset, obj);
